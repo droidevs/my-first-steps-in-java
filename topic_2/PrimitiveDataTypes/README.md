d59b6e3ac4686ba75697b9821c705bdc02e54d64
Mouad Oumous <mouadoumous@gmail.com>
2025-12-12 15:05:29 +0100

    primitive data types

---
+++ topic_2/PrimitiveDataTypes/README.md
@@ -0,0 +1,3 @@
+# Primitive Data Types
+
+This program demonstrates the 8 primitive data types in Java.
---
+++ topic_2/PrimitiveDataTypes/src/Main.java
@@ -0,0 +1,38 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Primitive Data Types
+        // byte
+        byte myByte = 100;
+        System.out.println("byte: " + myByte);
+
+        // short
+        short myShort = 5000;
+        System.out.println("short: " + myShort);
+
+        // int
+        int myInt = 65000;
+        System.out.println("int: " + myInt);
+
+        // long
+        long myLong = 15000000000L;
+        System.out.println("long: " + myLong);
+
+        // float
+        float myFloat = 5.75f;
+        System.out.println("float: " + myFloat);
+
+        // double
+        double myDouble = 19.99d;
+        System.out.println("double: " + myDouble);
+
+        // char
+        char myChar = 'A';
+        System.out.println("char: " + myChar);
+
+        // boolean
+        boolean myBoolean = true;
+        System.out.println("boolean: " + myBoolean);
+    }
+}
